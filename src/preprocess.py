14f4ef5b3e3afb7b42a080fb4c04ee88cbb1aac8
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-11-05 22:02:57 +0530

    scaling

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,25 @@
 """basic preprocessing: load csv, scale numeric features."""
 import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
 def load(path):
     df = pd.read_csv(path)
     return df
+
+
+def select_features(df, features):
+    return df[features].copy()
+
+
+def scale(X, method="standard"):
+    if method == "standard":
+        scaler = StandardScaler()
+    elif method == "minmax":
+        scaler = MinMaxScaler()
+    elif method == "none":
+        return X.values, None
+    else:
+        raise ValueError("unknown scale_method: %s" % method)
+    Xs = scaler.fit_transform(X)
+    return Xs, scaler

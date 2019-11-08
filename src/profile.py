4b579028c8d6e92e3472aab8db763149fad0b814
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-11-08 20:48:12 +0530

    wip 2

--- src/profile.py
+++ src/profile.py
@@ -6,3 +6,11 @@ def describe_clusters(df, labels):
     df = df.copy()
     df["cluster"] = labels
     return df
+
+
+def cluster_summary(df_with_labels, numeric_cols):
+    """return a dataframe with mean of numeric_cols and a count, per cluster."""
+    g = df_with_labels.groupby("cluster")
+    summary = g[numeric_cols].mean().round(2)
+    summary["count"] = g.size()
+    return summary.reset_index()

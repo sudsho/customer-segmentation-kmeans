1f157e95ee912e4d38840d015af024496f5c4701
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-11-13 21:25:39 +0530

    wip

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,9 +7,14 @@ import pandas as pd
 import plotly.express as px
 import joblib
 
+from src.preprocess import select_features, scale
+from src.cluster import elbow, silhouette_sweep
+from dashboard.figures import elbow_figure, silhouette_figure
+
 
 DATA_PATH = os.environ.get("DATA_PATH", "data/Mall_Customers.csv")
 MODEL_PATH = os.environ.get("MODEL_PATH", "artifacts/kmeans.joblib")
+FEATURES = ["Age", "Annual Income (k$)", "Spending Score (1-100)"]
 
 
 def load_data():
@@ -28,7 +33,7 @@ def load_data():
 
 
 df = load_data()
-fig = px.scatter(
+scatter = px.scatter(
     df,
     x="Annual Income (k$)",
     y="Spending Score (1-100)",
@@ -37,6 +42,12 @@ fig = px.scatter(
     title="customers by cluster",
 )
 
+# elbow + silhouette computed live for the dashboard
+X_raw = select_features(df, FEATURES)
+Xs, _ = scale(X_raw, "standard")
+elbow_data = elbow(Xs, 2, 10)
+sil_data = silhouette_sweep(Xs, 2, 10)
+
 
 app = dash.Dash(__name__)
 app.title = "mall customer segments"
@@ -44,7 +55,11 @@ server = app.server  # for gunicorn / heroku
 
 app.layout = html.Div([
     html.H1("mall customer segments"),
-    dcc.Graph(id="scatter", figure=fig),
+    dcc.Graph(id="scatter", figure=scatter),
+    html.Div([
+        dcc.Graph(id="elbow", figure=elbow_figure(elbow_data)),
+        dcc.Graph(id="silhouette", figure=silhouette_figure(sil_data)),
+    ]),
 ])
 
 

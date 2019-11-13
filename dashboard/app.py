47c806d79ba6fa4088241b938b259b1c27f34c97
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-11-13 18:58:47 +0530

    dashboard scatter colored by cluster

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,15 +1,50 @@
 """plotly dash app showing the segmentation results."""
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+import joblib
+
+
+DATA_PATH = os.environ.get("DATA_PATH", "data/Mall_Customers.csv")
+MODEL_PATH = os.environ.get("MODEL_PATH", "artifacts/kmeans.joblib")
+
+
+def load_data():
+    df = pd.read_csv(DATA_PATH)
+    if os.path.exists(MODEL_PATH):
+        bundle = joblib.load(MODEL_PATH)
+        scaler = bundle["scaler"]
+        feats = bundle["features"]
+        X = df[feats].values
+        if scaler is not None:
+            X = scaler.transform(X)
+        df["cluster"] = bundle["model"].predict(X).astype(str)
+    else:
+        df["cluster"] = "0"
+    return df
+
+
+df = load_data()
+fig = px.scatter(
+    df,
+    x="Annual Income (k$)",
+    y="Spending Score (1-100)",
+    color="cluster",
+    hover_data=["Age", "Gender"],
+    title="customers by cluster",
+)
 
 
 app = dash.Dash(__name__)
 app.title = "mall customer segments"
+server = app.server  # for gunicorn / heroku
 
 app.layout = html.Div([
     html.H1("mall customer segments"),
-    html.P("dashboard wip"),
+    dcc.Graph(id="scatter", figure=fig),
 ])
 
 

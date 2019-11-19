9ee4bea2bd71847bc3bd0d689f733451ba009aca
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-11-19 23:10:15 +0530

    persona labels for each cluster

--- src/profile.py
+++ src/profile.py
@@ -14,3 +14,24 @@ def cluster_summary(df_with_labels, numeric_cols):
     summary = g[numeric_cols].mean().round(2)
     summary["count"] = g.size()
     return summary.reset_index()
+
+
+def cluster_label(row, income_col="Annual Income (k$)", spend_col="Spending Score (1-100)"):
+    """human-readable persona name based on income+spending mean."""
+    inc = row[income_col]
+    spend = row[spend_col]
+    if inc >= 70 and spend >= 60:
+        return "premium"
+    if inc >= 70 and spend < 40:
+        return "careful high-income"
+    if inc < 40 and spend >= 60:
+        return "young splurger"
+    if inc < 40 and spend < 40:
+        return "thrifty"
+    return "average"
+
+
+def add_personas(summary, **kwargs):
+    summary = summary.copy()
+    summary["persona"] = summary.apply(lambda r: cluster_label(r, **kwargs), axis=1)
+    return summary
